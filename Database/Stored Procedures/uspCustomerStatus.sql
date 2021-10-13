26b8c9ea4a09be020b6d33164b69b9663536a2e5
Jon Nelson <jon.r.nelson@gmail.com>
2021-10-13 12:50:56 -0600

    updated lost to use parameter instead of system date

--- Database/Stored Procedures/uspCustomerStatus.sql
+++ Database/Stored Procedures/uspCustomerStatus.sql
@@ -27,13 +27,13 @@ FROM(
 						   ,[LastName]
 						   ,[Active]   = CASE WHEN S.[TransactionDate] BETWEEN  @StartOfMonth AND @MonthEnd AND S.[SaleAmount] > 0 THEN 1 ELSE 0 END
 						   ,[New]      = CASE WHEN C.[InsertDate]      BETWEEN  @StartOfMonth AND @MonthEnd THEN 1 ELSE 0 END
-						   ,[Lost]     = CASE WHEN S.[TransactionDate] < DATEADD(MONTH, -6, GETDATE())      THEN 1 ELSE 0 END
+						   ,[Lost]     = CASE WHEN S.[TransactionDate] < DATEADD(MONTH, -6, @MonthEnd)      THEN 1 ELSE 0 END
 			FROM	[dbo].[Customer] C
 			JOIN	[dbo].[Sales] S
 					ON S.[CustomerId] = C.[CustomerId]
 			WHERE  (S.[TransactionDate] BETWEEN  @StartOfMonth AND @MonthEnd AND S.[SaleAmount] > 0)
 			OR     (C.[InsertDate]      BETWEEN  @StartOfMonth AND @MonthEnd)
-			OR     (S.[TransactionDate] < DATEADD(MONTH, -6, GETDATE()))
+			OR     (S.[TransactionDate] < DATEADD(MONTH, -6, @MonthEnd))
             )temp
        GROUP BY 
        	[CustomerId]
