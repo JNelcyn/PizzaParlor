6c85042e3bc7f7b1afb5994861ed60b1cc57d50f
JNelcyn <92384639+JNelcyn@users.noreply.github.com>
2021-10-14 08:37:31 -0600

    Update uspCustomerStatus.sql
    Indented some code that was out of line.

--- Database/Stored Procedures/uspCustomerStatus.sql
+++ Database/Stored Procedures/uspCustomerStatus.sql
@@ -15,22 +15,22 @@ SET @StartOfMonth = (SELECT DATEADD(DAY, 1, EOMONTH(@MonthEnd, -1)))
 ,[Reclaim] = CASE WHEN [Active] = 1 AND [Lost] = 1 THEN 1 ELSE 0 END
 FROM(  
      SELECT 
-     [CustomerId] 
+     	 [CustomerId] 
 	,[FirstName]
 	,[LastName]
 	,[Active] = SUM([Active])
 	,[New]    = SUM([New])
 	,[Lost]   = SUM([Lost])
     FROM( 
-		  SELECT DISTINCT [CustomerId] = C.[CustomerId]
-						 ,[FirstName]
-						 ,[LastName]
-						 ,[Active]   = CASE WHEN S.[TransactionDate] BETWEEN  @StartOfMonth AND @MonthEnd AND S.[SaleAmount] > 0 THEN 1 ELSE 0 END
-						 ,[New]      = CASE WHEN C.[InsertDate]      BETWEEN  @StartOfMonth AND @MonthEnd THEN 1 ELSE 0 END
-						 ,[Lost]     = CASE WHEN S.[TransactionDate] < DATEADD(MONTH, -6, @MonthEnd)      THEN 1 ELSE 0 END
+		  SELECT DISTINCT     [CustomerId] = C.[CustomerId]
+			             ,[FirstName]
+				     ,[LastName]
+				     ,[Active]     = CASE WHEN S.[TransactionDate] BETWEEN  @StartOfMonth AND @MonthEnd AND S.[SaleAmount] > 0 THEN 1 ELSE 0 END
+				     ,[New]        = CASE WHEN C.[InsertDate]      BETWEEN  @StartOfMonth AND @MonthEnd THEN 1 ELSE 0 END
+				     ,[Lost]     = CASE WHEN S.[TransactionDate] < DATEADD(MONTH, -6, @MonthEnd)      THEN 1 ELSE 0 END
 			FROM	[dbo].[Customer] C
 			JOIN	[dbo].[Sales] S
-					ON S.[CustomerId] = C.[CustomerId]
+				ON S.[CustomerId] = C.[CustomerId]
 			WHERE  (S.[TransactionDate] BETWEEN  @StartOfMonth AND @MonthEnd AND S.[SaleAmount] > 0)
 			OR     (C.[InsertDate]      BETWEEN  @StartOfMonth AND @MonthEnd)
 			OR     (S.[TransactionDate] < DATEADD(MONTH, -6, @MonthEnd))
