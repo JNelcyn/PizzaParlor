bea4a374189e90afaabd5cf9ed2996fae810f1d8
JNelcyn <92384639+JNelcyn@users.noreply.github.com>
2021-10-14 08:41:57 -0600

    Update Sales.sql
    Added PK constraint code so the PK does not auto generate a name.

--- Database/Schema/Sales.sql
+++ Database/Schema/Sales.sql
@@ -4,12 +4,10 @@ CREATE TABLE [dbo].[Sales]
     [CustomerId]       [INT]            NOT NULL, 
     [SaleAmount]       [Decimal] (19,2) NOT NULL,
     [TransactionDate]  [Datetime]       NOT NULL,
-PRIMARY KEY ([SalesID]),
+CONSTRAINT [PK_Sales_SalesID] PRIMARY KEY ([SalesID] ASC),
 CONSTRAINT [FK_Sales_CustomerId]  FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer] ([CustomerId])
 );
 GO 
 CREATE NONCLUSTERED INDEX [NCIX_Sales]
 	ON [dbo].[Sales] ([CustomerId],[SalesId])
 	INCLUDE ([TransactionDate],[SaleAmount])
-
-
